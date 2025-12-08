6b576aa5615bde18e8e05033e5adf504015e50c6
Sanjay Pokkali <sanjay.pokkali99@gmail.com>
2025-12-08 13:48:01 -0600

    Fix user api

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -18,101 +18,119 @@ const verifyToken = (req, res, next) => {
 
 module.exports = (router) => {
     router.route('/adopterUsers/register')
-        .post((req, res) => {
-            var adopterUser = new AdopterUser();
-            adopterUser.name = req.body.name;
-            adopterUser.email = req.body.email;
-            adopterUser.role = req.body.role;
-            adopterUser.location = req.body.location;
-            adopterUser.password = req.body.password;
-            adopterUser.save((err) => {
-                if (err) return res.status(400).send(err);
+        .post(async (req, res) => {
+            try {
+                var adopterUser = new AdopterUser();
+                adopterUser.name = req.body.name;
+                adopterUser.email = req.body.email;
+                adopterUser.role = req.body.role;
+                adopterUser.location = req.body.location;
+                adopterUser.password = req.body.password;
+                await adopterUser.save();
                 const token = jwt.sign({ id: adopterUser._id }, JWT_SECRET, { expiresIn: '24h' });
                 res.status(201).json({ message: 'Adopter user created!', user: adopterUser, token });
-            });
+            } catch (err) {
+                res.status(400).send(err);
+            }
         });
     
     router.route('/adopterUsers/login')
-        .post((req, res) => {
-            AdopterUser.findOne({ email: req.body.email }, (err, user) => {
-                if (err) return res.status(500).send(err);
+        .post(async (req, res) => {
+            try {
+                const user = await AdopterUser.findOne({ email: req.body.email });
                 if (!user) return res.status(401).json({ message: 'User not found' });
                 if (user.password !== req.body.password) {
                     return res.status(401).json({ message: 'Invalid password' });
                 }
                 const token = jwt.sign({ id: user._id }, JWT_SECRET, { expiresIn: '24h' });
                 res.status(200).json({ message: 'Login successful', user: user, token });
-            });
+            } catch (err) {
+                res.status(500).send(err);
+            }
         });
     
     router.route('/adopterUsers')
-        .get(verifyToken, (req, res) => {
-            AdopterUser.find((err, users) => {
-                if (err) return res.status(500).send(err);
+        .get(verifyToken, async (req, res) => {
+            try {
+                const users = await AdopterUser.find();
                 res.status(200).json(users);
-            });
+            } catch (err) {
+                res.status(500).send(err);
+            }
         });
     
     router.route('/adopterUsers/:id')
-        .get(verifyToken, (req, res) => {
-            AdopterUser.findById(req.params.id, (err, user) => {
-                if (err) return res.status(500).send(err);
+        .get(verifyToken, async (req, res) => {
+            try {
+                const user = await AdopterUser.findById(req.params.id);
                 if (!user) return res.status(404).json({ message: 'User not found' });
                 res.status(200).json(user);
-            });
+            } catch (err) {
+                res.status(500).send(err);
+            }
         });
     
     router.route('/adopterUsers/:id')
-        .put(verifyToken, (req, res) => {
-            AdopterUser.findById(req.params.id, (err, user) => {
-                if (err) return res.status(500).send(err);
+        .put(verifyToken, async (req, res) => {
+            try {
+                const user = await AdopterUser.findById(req.params.id);
                 if (!user) return res.status(404).json({ message: 'User not found' });
                 user.name = req.body.name || user.name;
                 user.email = req.body.email || user.email;
                 user.location = req.body.location || user.location;
-                user.save((err) => {
-                    if (err) return res.status(400).send(err);
-                    res.status(200).json({ message: 'User updated!', user: user });
-                });
-            });
+                await user.save();
+                res.status(200).json({ message: 'User updated!', user: user });
+            } catch (err) {
+                if (err.name === 'CastError') {
+                    res.status(500).send(err);
+                } else {
+                    res.status(400).send(err);
+                }
+            }
         });
     
     router.route('/adopterUsers/:id')
-        .delete(verifyToken, (req, res) => {
-            AdopterUser.remove({ _id: req.params.id }, (err) => {
-                if (err) return res.status(500).send(err);
+        .delete(verifyToken, async (req, res) => {
+            try {
+                await AdopterUser.deleteOne({ _id: req.params.id });
                 res.status(200).json({ message: 'User deleted!' });
-            });
+            } catch (err) {
+                res.status(500).send(err);
+            }
         });
     
     router.route('/shelterUsers/register')
-        .post((req, res) => {
-            var shelterUser = new ShelterUser();
-            shelterUser.name = req.body.name;
-            shelterUser.email = req.body.email;
-            shelterUser.role = req.body.role;
-            shelterUser.address = req.body.address;
-            shelterUser.phone = req.body.phone;
-            shelterUser.website = req.body.website;
-            shelterUser.password = req.body.password;
-            shelterUser.save((err) => {
-                if (err) return res.status(400).send(err);
+        .post(async (req, res) => {
+            try {
+                var shelterUser = new ShelterUser();
+                shelterUser.name = req.body.name;
+                shelterUser.email = req.body.email;
+                shelterUser.role = req.body.role;
+                shelterUser.address = req.body.address;
+                shelterUser.phone = req.body.phone;
+                shelterUser.website = req.body.website;
+                shelterUser.password = req.body.password;
+                await shelterUser.save();
                 const token = jwt.sign({ id: shelterUser._id}, JWT_SECRET, { expiresIn: '24h' });
                 res.status(201).json({ message: 'Shelter user created!', user: shelterUser, token });
-            });
+            } catch (err) {
+                res.status(400).send(err);
+            }
         });
     
     router.route('/shelterUsers/login')
-        .post((req, res) => {
-            ShelterUser.findOne({ email: req.body.email }, (err, user) => {
-                if (err) return res.status(500).send(err);
+        .post(async (req, res) => {
+            try {
+                const user = await ShelterUser.findOne({ email: req.body.email });
                 if (!user) return res.status(401).json({ message: 'User not found' });
                 if (user.password !== req.body.password) {
                     return res.status(401).json({ message: 'Invalid password' });
                 }
                 const token = jwt.sign({ id: user._id}, JWT_SECRET, { expiresIn: '24h' });
                 res.status(200).json({ message: 'Login successful', user: user, token });
-            });
+            } catch (err) {
+                res.status(500).send(err);
+            }
         });
     
     return router;
